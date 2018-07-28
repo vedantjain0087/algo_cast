2f29eb5e50ab9497ab64325e65690538616994dc
Vedant <mainvedant.vns123@gmail.com>
2018-07-28 17:23:41 +0530

    reveseInt

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charMap = {};
+    let max = 0;
+    let maxChar = '';
+    for(let ch of str){
+        charMap[ch] = charMap[ch] + 1 || 1;
+    }
+
+    for(let key in charMap){
+        if(charMap[key] > max){
+            max = charMap[key];
+            maxChar = key
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
