e44a5acc8afe92b69f0c68eca4f6d4ce113ee587
Vedant <mainvedant.vns123@gmail.com>
2018-07-30 16:56:02 +0530

    capitalize

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+    let new_w = [];
+
+    for(let ch of str.split(' ')){
+        new_w.push(ch.slice(0,1).toUpperCase() + ch.slice(1));
+    }
+
+    return new_w.join(' ');
+}
 
 module.exports = capitalize;
